835b6b8ed3a44f6da042c00a74f827ca65f77af9
PLAKA <mijodecao96@gmail.com>
2018-05-11 01:54:57 +0100

    mais 1

--- ARCVDS_Final/Models/Eventos.cs
+++ ARCVDS_Final/Models/Eventos.cs
@@ -66,6 +66,5 @@ namespace ARCVDS_Final.Models {
 
         }
 
-
     }
 }
--- ARCVDS_Final/Startup.cs
+++ ARCVDS_Final/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using ARCVDS_Final.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ARCVDS_Final.Startup))]
@@ -10,5 +13,74 @@ namespace ARCVDS_Final
         {
             ConfigureAuth(app);
         }
+
+
+        private void iniciaRoles () {
+
+            ApplicationDbContext db = new ApplicationDbContext ();
+
+            var roleManager = new RoleManager<IdentityRole> (new RoleStore<IdentityRole> (db));
+            var userManager = new UserManager<ApplicationUser> (new UserStore<ApplicationUser> (db));
+
+            if (!roleManager.RoleExists("Funcionarios")) {
+
+                var roleFun = new IdentityRole ();
+                roleFun.Name = "Funcionarios";
+                roleManager.Create (roleFun);
+
+                var user = new ApplicationUser ();
+                user.UserName = "Jorge";
+                user.Email = "jorge@gmail.com";
+
+                string jorgePassword = "123";
+
+                var chkUser = userManager.Create (user,jorgePassword);
+
+                if (chkUser.Succeeded) {
+                    var result1 = userManager.AddToRole (user.Id,"Funcionarios");
+                }
+
+            }
+
+            if (!roleManager.RoleExists("Socios")) {
+
+                var roleSocio = new IdentityRole ();
+                roleSocio.Name = "Socios";
+                roleManager.Create (roleSocio);
+
+                var user = new ApplicationUser ();
+                user.UserName = "Luis";
+                user.Email = "luis@gmail.com";
+
+                string luisPassword = "123";
+
+                var chkUser = userManager.Create (user,luisPassword);
+
+                if (chkUser.Succeeded) {
+                    var result1 = userManager.AddToRole (user.Id,"Socios");
+                }
+            }
+
+            if (!roleManager.RoleExists ("Admin")) {
+
+                var roleAdmin = new IdentityRole ();
+                roleAdmin.Name = "Admin";
+                roleManager.Create (roleAdmin);
+
+                var user = new ApplicationUser ();
+                user.UserName = "plaka";
+                user.Email = "plaka@gmail.com";
+
+                string plakaPassword = "123";
+                var chkUser = userManager.Create (user,plakaPassword);
+
+                if (chkUser.Succeeded) {
+                    var result1 = userManager.AddToRole (user.Id,"Admin");
+                }
+
+            }
+
+        }
+
     }
 }
